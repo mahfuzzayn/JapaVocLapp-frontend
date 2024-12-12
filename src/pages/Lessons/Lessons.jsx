d53354726ba01c32bb24a0b5bd4e4c0864d649e6
mahfuzzayn <mahfuzzayn8@gmail.com>
2024-12-12 22:13:53 +0600

    Integrated React Confetti

--- src/pages/Lessons/Lessons.jsx
+++ src/pages/Lessons/Lessons.jsx
@@ -44,7 +44,7 @@ const Lessons = () => {
     return (
         <div className="lessons p-8">
             <h1 className="text-3xl font-bold mb-6 text-cyan-500">
-                Lessons Page
+                Available Lessons
             </h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 {lessons.map((lesson) => (
@@ -52,18 +52,26 @@ const Lessons = () => {
                         key={lesson._id}
                         className="bg-white p-4 rounded-lg shadow-md"
                     >
-                        <h2 className="text-xl font-semibold">{lesson.name}</h2>
-                        <p className="text-sm text-gray-600">
-                            Lesson Number: {lesson.lessonNumber}
+                        <h2 className="text-3xl font-semibold mb-4">
+                            {lesson.name}
+                        </h2>
+                        <p className="text-lg text-gray-600">
+                            Lesson Number:
+                            <div className="badge badge-warning ml-1">
+                                {lesson.lessonNumber}
+                            </div>
                         </p>
-                        <p className="text-sm text-gray-600">
-                            Vocabulary Count: {lesson.vocabularyCount}
+                        <p className="text-lg text-gray-600">
+                            Vocabulary Count:
+                            <div className="badge badge-info ml-1">
+                                {lesson.vocabularyCount}
+                            </div>
                         </p>
                         <button
                             onClick={() =>
                                 handleLessonClick(lesson.lessonNumber)
                             }
-                            className="btn btn-primary mt-4"
+                            className="btn btn-primary text-white text-lg mt-4"
                         >
                             Study Lesson
                         </button>
