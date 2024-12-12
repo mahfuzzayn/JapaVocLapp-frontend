d1320026ffac807e7f2c81c47866acadd2ff8838
mahfuzzayn <mahfuzzayn8@gmail.com>
2024-12-12 19:16:50 +0600

    Added Googel Font: Inter

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 const Home = () => {
     return (
         <div className="text-3xl">
+            <h1 className="text-2xl font-bold">JVLA</h1>
             <Button variant="primary">Button</Button>
         </div>
     );
